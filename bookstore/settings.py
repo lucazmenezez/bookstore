d121ebc773d3fdf2e06219ae7a00d0980444ae0d
lucas menezes <menezeslucas24082002@gmail.com>
2025-09-05 15:39:18 -0300

    feat: test code review automation

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -20,14 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!rnfh2f@ed!=pv%ct#=%-km*nmf#=k)sbhi5&fzix7m1^_o@m6"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
